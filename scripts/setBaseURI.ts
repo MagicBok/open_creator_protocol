2651af31e56aa2db9f3bb213ee41866fec63dab0
Tim Huynh <tim@magiceden.io>
2023-04-24 14:10:39 +1000

    set gasprice for setBaseUri

--- scripts/setBaseURI.ts
+++ scripts/setBaseURI.ts
@@ -4,6 +4,7 @@ import { ContractDetails } from './common/constants';
 export interface ISetBaseURIParams {
   uri: string;
   contract: string;
+  gasprice?: number;
 }
 
 export const setBaseURI = async (
@@ -11,9 +12,14 @@ export const setBaseURI = async (
   hre: HardhatRuntimeEnvironment,
 ) => {
   const { ethers } = hre;
+  let overrides: any = {};
+
+  if (args.gasprice) {
+    overrides.gasPrice = args.gasprice * 1e9;
+  }
   const ERC721M = await ethers.getContractFactory(ContractDetails.ERC721M.name);
   const contract = ERC721M.attach(args.contract);
-  const tx = await contract.setBaseURI(args.uri);
+  const tx = await contract.setBaseURI(args.uri, overrides);
   console.log(`Submitted tx ${tx.hash}`);
 
   await tx.wait();
